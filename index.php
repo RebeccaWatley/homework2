fa1269016620b5eaad482fd3f259a552d5abdafc
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-09-18 17:39:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,13 @@ E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
+<form action="handleget.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
