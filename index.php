7ecc83c2273437adda82fcf21a02c04f7424e045
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-09-26 13:46:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Response</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-  </head>
-  <body>
+
+     <?php require_once("header.php"); ?>
     <h1>My Website</h1>
-  
- <?php require_once("header.php"); ?>
+
 <form action="handlepost.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
