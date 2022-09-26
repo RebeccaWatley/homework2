9496de94ae5ff42f9bfeff4dce2e3c42fab4089e
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-09-26 14:08:39 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -6,8 +6,9 @@
     <title>Response</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
+    <?php require_once("header.php"); ?>
+    <h1>My Website</h1>
   <body>
-    <h1>Post Response</h1>
     Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
